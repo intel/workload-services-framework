9d7a8eca9ec6a8b02df0af93be99d3b65f877360
Pavan Savanoor <pavan.m.savanoor@intel.com>
2024-12-13 17:02:32 +0100

    added missing .tf files

---
+++ script/terraform/terraform-config.alicloud.tf
@@ -0,0 +1,190 @@
+#
+# Apache v2 license
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+variable "disk_spec_1" {
+  default = {
+    disk_count = 1
+    disk_size = 200
+    disk_format = "ext4"
+    disk_type = "cloud_essd"
+    disk_performance = null
+  }
+}
+
+variable "disk_spec_2" {
+  default = {
+    disk_count = 1
+    disk_size = 200
+    disk_format = "ext4"
+    disk_type = "cloud_essd"
+    disk_performance = null
+  }
+}
+
+variable "region" {
+  default = null
+}
+
+variable "zone" {
+  default = "cn-beijing-l"
+}
+
+variable "resource_group_id" {
+  default = "rg-aekzpvcftlcma5a"
+}
+
+variable "owner" {
+  default = ""
+}
+
+variable "custom_tags" {
+  default = {}
+}
+
+variable "spot_instance" {
+  default = true
+}
+
+variable "wl_name" {
+  default = ""
+}
+
+variable "wl_registry_map" {
+  default = ""
+}
+
+variable "wl_namespace" {
+  default = ""
+}
+
+variable "worker_profile" {
+  default = {
+    name = "worker"
+    instance_type = "ecs.g6.large"
+    cpu_model_regex = null
+    vm_count = 1
+
+    os_image = null
+    os_type = "ubuntu2204"
+    os_disk_type = "cloud_essd"
+    os_disk_size = 200
+    os_disk_performance = null
+
+    data_disk_spec = null
+  }
+}
+
+variable "client_profile" {
+  default = {
+    name = "client"
+    instance_type = "ecs.g6.large"
+    cpu_model_regex = null
+    vm_count = 1
+
+    os_image = null
+    os_type = "ubuntu2204"
+    os_disk_type = "cloud_essd"
+    os_disk_size = 200
+    os_disk_performance = null
+
+    data_disk_spec = null
+  }
+}
+
+variable "controller_profile" {
+  default = {
+    name = "controller"
+    instance_type = "ecs.g6.large"
+    cpu_model_regex = null
+    vm_count = 1
+
+    os_image = null
+    os_type = "ubuntu2204"
+    os_disk_type = "cloud_essd"
+    os_disk_size = 200
+    os_disk_performance = null
+
+    data_disk_spec = null
+  }
+}
+
+module "wsf" {
+  source = "./template/terraform/alicloud/main"
+
+  region = var.region
+  zone = var.zone
+  resource_group_id = var.resource_group_id
+  job_id = var.wl_namespace
+
+  sg_whitelist_cidr_blocks = compact(split("\n",file("proxy-ip-list.txt")))
+  ssh_pub_key = file("ssh_access.key.pub")
+
+  common_tags = {
+    for k,v in merge(var.custom_tags, {
+      owner: var.owner,
+      workload: var.wl_name,
+    }) : k => substr(replace(lower(v), "/[^a-z0-9_-]/", ""), 0, 63)
+  }
+
+  instance_profiles = [
+    merge(var.worker_profile, {
+      data_disk_spec: null,
+    }),
+    merge(var.client_profile, {
+      data_disk_spec: null,
+    }),
+    merge(var.controller_profile, {
+      data_disk_spec: null,
+    }),
+  ]
+
+  spot_instance = var.spot_instance
+}
+
+output "options" {
+  value = {
+    wl_name : var.wl_name,
+    wl_registry_map : var.wl_registry_map,
+    wl_namespace : var.wl_namespace,
+
+    docker_dist_repo: "https://mirrors.aliyun.com/docker-ce",
+    containerd_pause_registry: "registry.aliyuncs.com/google_containers",
+
+    k8s_repo_key_url: {
+      "debian": "http://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg",
+      "centos": ["http://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg","https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg"],
+    },
+    k8s_repo_url: {
+      "debian": "http://mirrors.aliyun.com/kubernetes/apt kubernetes-xenial main",
+      "centos": "http://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-$basearch",
+    },
+    k8s_kubeadm_options: {
+      "ClusterConfiguration": {
+        "imageRepository": "registry.aliyuncs.com/google_containers",
+      },
+    },
+    k8s_registry_image: "public.ecr.aws/docker/library/registry:2",
+    k8s_calico_cni_repo: "public.ecr.aws/metakube/calico",
+    k8s_cni: "calico",
+    k8s_enable_nfd: false,
+  }
+}
+
+output "instances" {
+  value = {
+    for k,v in module.wsf.instances : k => merge(v, {
+      csp = "alicloud",
+      zone = var.zone,
+    })
+  }
+}
+
+output "terraform_replace" {
+  value = lookup(module.wsf, "terraform_replace", null)==null?null:{
+    command = replace(module.wsf.terraform_replace.command, "=", "=module.wsf.")
+    cpu_model = module.wsf.terraform_replace.cpu_model
+  }
+}
---
+++ script/terraform/terraform-config.kvm.tf
@@ -0,0 +1,255 @@
+#
+# Apache v2 license
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+variable "disk_spec_1" {
+  default = {
+    disk_count = 1
+    disk_size = 100
+    disk_format = "ext4"
+  }
+}
+
+variable "disk_spec_2" {
+  default = {
+    disk_count = 1
+    disk_size = 200
+    disk_format = "ext4"
+  }
+}
+
+variable "network_spec_1" {
+  default = {
+    network_count = 1
+    network_type = null
+  }
+}
+
+variable "owner" {
+  default = ""
+}
+
+variable "wl_name" {
+  default = ""
+}
+
+variable "wl_namespace" {
+  default = ""
+}
+
+variable "wl_registry_map" {
+  default = ""
+}
+
+variable "worker_profile" {
+  default = {
+    name = "worker"
+    cpu_core_count = 2
+    memory_size = 2
+    vm_count = 1
+
+    os_image = null
+    os_type = "ubuntu2204"
+    os_disk_size = 50
+
+    data_disk_spec = null
+    network_spec = null
+
+    # Specifies which KVM host must be used to host
+    # different workers.
+    kvm_hosts = [ 0, 1, 2, 0, 1, 2 ]
+  }
+}
+
+variable "client_profile" {
+  default = {
+    name = "client"
+    cpu_core_count = 2
+    memory_size = 2
+    vm_count = 1
+
+    os_image = null
+    os_type = "ubuntu2204"
+    os_disk_size = 50
+
+    data_disk_spec = null
+    network_spec = null
+
+    # Specifies which KVM host must be used to host
+    # different clients.
+    kvm_hosts = [ 1, 0, 1, 2, 0, 1 ]
+  }
+}
+
+variable "controller_profile" {
+  default = {
+    name = "controller"
+    cpu_core_count = 2
+    memory_size = 2
+    vm_count = 1
+
+    os_image = null
+    os_type = "ubuntu2204"
+    os_disk_size = 50
+
+    data_disk_spec = null
+    network_spec = null
+
+    # Specifies which KVM host must be used to host
+    # different controllers.
+    kvm_hosts = [ 1, 2, 0, 1, 2, 0 ]
+  }
+}
+
+# single KVM host
+variable "kvm_hosts" {
+  default = [{
+    user   = "user"
+    host   = "127.0.1.1"
+    port   = 22
+    # DHCP must be enabled on the network interface
+    networks = [ "default" ]
+    # if specified, os image will reuse the storage pool
+    # images (with same image names.)
+    pool   = null
+  }]
+}
+
+# multiple KVM hosts
+#variable "kvm_hosts" {
+#  default = [{
+#    user = "user"
+#    host = "127.0.1.1"
+#    port = 22
+#    # DHCP must be enabled on the network interface
+#    networks = [ "wsfbr0", "default" ]
+#    # if specified, os image will reuse the storage pool
+#    # images (with same image names.)
+#    pool = "osimages"
+#  }, {
+#    user = "user"
+#    host = "127.0.1.2"
+#    port = 22
+#    # DHCP must be enabled on the network interface
+#    networks = [ "wsfbr0", "default" ]
+#    # if specified, os image will reuse the storage pool
+#    # images (with same image names.)
+#    pool = "osimages"
+#  }]
+#}
+
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu+ssh://${var.kvm_hosts.0.user}@${var.kvm_hosts.0.host}:${var.kvm_hosts.0.port}/system"
+  alias = "kvm0"
+}
+
+provider "libvirt" {
+  uri = "qemu+ssh://${element(var.kvm_hosts,1).user}@${element(var.kvm_hosts,1).host}:${element(var.kvm_hosts,1).port}/system"
+  alias = "kvm1"
+}
+
+provider "libvirt" {
+  uri = "qemu+ssh://${element(var.kvm_hosts,2).user}@${element(var.kvm_hosts,2).host}:${element(var.kvm_hosts,2).port}/system"
+  alias = "kvm2"
+}
+
+locals {
+  instance_profiles = [
+    merge(var.worker_profile, {
+      data_disk_spec: null,
+      network_spec: null,
+    }),
+    merge(var.client_profile, {
+      data_disk_spec: null
+      network_spec: null,
+    }),
+    merge(var.controller_profile, {
+      data_disk_spec: null,
+      network_spec: null,
+    }),
+  ]
+}
+
+module "wsf_kvm0" {
+  source = "./template/terraform/kvm/local"
+
+  job_id = var.wl_namespace
+  ssh_pub_key = file("${path.root}/ssh_access.key.pub")
+  instance_profiles = local.instance_profiles
+
+  kvm_index = 0
+  kvm_host  = var.kvm_hosts.0
+  providers = {
+    libvirt = libvirt.kvm0
+  }
+}
+
+module "wsf_kvm1" {
+  source = "./template/terraform/kvm/local"
+
+  job_id = var.wl_namespace
+  ssh_pub_key = file("${path.root}/ssh_access.key.pub")
+  instance_profiles = local.instance_profiles
+
+  kvm_index = 1
+  kvm_host  = element(var.kvm_hosts,1)
+  providers = {
+    libvirt = libvirt.kvm1
+  }
+}
+
+module "wsf_kvm2" {
+  source = "./template/terraform/kvm/local"
+
+  job_id = var.wl_namespace
+  ssh_pub_key = file("${path.root}/ssh_access.key.pub")
+  instance_profiles = local.instance_profiles
+
+  kvm_index = 2
+  kvm_host  = element(var.kvm_hosts,2)
+  providers = {
+    libvirt = libvirt.kvm2
+  }
+}
+
+output "options" {
+  value = merge({
+    wl_name : var.wl_name,
+    wl_registry_map : var.wl_registry_map,
+    wl_namespace : var.wl_namespace,
+    k8s_enable_registry: true,
+  }, {
+    for k,v in try(module.wsf_kvm0.options,{}): k => v
+  }, {
+    for k,v in try(module.wsf_kvm1.options,{}): k => v
+  }, {
+    for k,v in try(module.wsf_kvm2.options,{}): k => v
+  })
+}
+
+output "instances" {
+  sensitive = true
+  value = merge({
+    for k,v in module.wsf_kvm0.instances : k => merge(v, {
+      csp = "kvm",
+    })
+  }, {
+    for k,v in module.wsf_kvm1.instances : k => merge(v, {
+      csp = "kvm",
+    })
+  }, {
+    for k,v in module.wsf_kvm2.instances : k => merge(v, {
+      csp = "kvm",
+    })
+  })
+}
+
